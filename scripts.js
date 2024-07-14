d0a875bb9e6a82362015a4bd1ec419fabc21a5a0
SarthakTechie <sarthaktechie@proton.me>
2024-07-14 22:11:43 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Filter out the project you want to ignore
             const filteredProjects = data.filter(repo => repo.name !== 'SarthakTechie');
+            const filteredProjects = data.filter(repo => repo.name !== 'SarthakTechie.github.io');
             
             filteredProjects.forEach(repo => {
                 const projectElement = document.createElement('div');
